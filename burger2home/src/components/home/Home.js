32b368fbd6a4d113d98d8f94105582960ea1f925
NathanSepul <sepul.nathan@gmail.com>
2022-11-25 09:49:50 +0100

    restructuration image

--- burger2home/src/components/home/Home.js
+++ burger2home/src/components/home/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
     <main>
       <title>Burger2Home</title>
       <Banniere />
-      <Area2 titre={t('accueil.area2.titre')} img1={"/picture/formidable.jpeg"} img2={"/picture/it.jpg"} img3={"/picture/fondant.jpg"} txt1={t('accueil.area2.txt1')} txt2={t('accueil.area2.txt2')} txt3={t('accueil.area2.txt3')} name="lesStars" />
+      <Area2 titre={t('accueil.area2.titre')} img1={"/picture/burger/formidable.jpeg"} img2={"/picture/burger/it.jpg"} img3={"/picture/burger/fondant.jpg"} txt1={t('accueil.area2.txt1')} txt2={t('accueil.area2.txt2')} txt3={t('accueil.area2.txt3')} name="lesStars" />
       <Area1 />
       <Area2 titre={t('accueil.area3.titre')} img1={"/picture/preparer_minute.jpg"} img2={"/picture/frais_du_jour.jpg"} img3={"/picture/a_la_main.jpg"} txt1={t('accueil.area3.txt1')} txt2={t('accueil.area3.txt2')} txt3={t('accueil.area3.txt3')} name="enPratique" />
     </main>
