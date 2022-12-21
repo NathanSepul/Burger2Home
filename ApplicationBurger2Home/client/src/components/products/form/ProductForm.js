eee76c453a4fbce8aee54a79ccca66f62b51d3db
NathanSepul <sepul.nathan@gmail.com>
2022-12-21 15:45:39 +0100

    form burger admin v1

--- ApplicationBurger2Home/client/src/components/products/form/ProductForm.js
+++ ApplicationBurger2Home/client/src/components/products/form/ProductForm.js
@@ -2,16 +2,47 @@ import React, { useState, useEffect } from "react"
 import Box from '@mui/material/Box';
 import Button from "@mui/material/Button";
 import TextField from '@mui/material/TextField';
-import axios from 'axios';
 import OutlinedInput from '@mui/material/OutlinedInput';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import axios from 'axios';
+
+import { useSelector } from 'react-redux';
 import "./ProductForm.css"
 const ProductForm = ({ productSelected }) => {
 
     const [productEn, setPorductEn] = useState({ id: "", name: "", description: "", currentPrice: "", currentDiscount: "", imageUrl: "", ingredients: [], allergens: [] });
     const [productFr, setProductFr] = useState({ id: "", name: "", description: "", currentPrice: "", currentDiscount: "", imageUrl: "", ingredients: [], allergens: [] });
     const [isDisabled, setIsDisabled] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     const [errorPrice, setErrorPrice] = useState(false);
-    
+    const languageRedux = useSelector(state => state.language)
+
+    const [ingredrientsList, setIngredientsList] = useState();
+    const [famillyList, setFamillyList] = useState();
+
+    ;
+
+    useEffect(() => {
+
+        const requestOne = axios.get(`/ingredients/translations?language=${languageRedux.value}`);
+        const requestTwo = axios.get(`/products/families/translations?language=${languageRedux.value}`);
+
+        axios.all([requestOne, requestTwo])
+            .then(
+                axios.spread((...responses) => {
+                    setIngredientsList(responses[0])
+                    setFamillyList(responses[1])
+                    setIsLoading(false)
+                })
+            )
+            .catch(e => {
+                console.error(e);
+            });
+
+    }, [languageRedux.value])
+
     useEffect(() => {
         if (productSelected.id !== "") {
             const requestOne = axios.get(`/products/summaries/${productSelected.id}?language=EN`);
@@ -20,8 +51,8 @@ const ProductForm = ({ productSelected }) => {
             axios.all([requestOne, requestTwo])
                 .then(
                     axios.spread((...responses) => {
-                        setPorductEn(responses[0].data)
-                        setProductFr(responses[1].data)
+                        setPorductEn(responses[0])
+                        setProductFr(responses[1])
                     })
                 )
                 .catch(e => {
@@ -32,16 +63,35 @@ const ProductForm = ({ productSelected }) => {
     }, [productSelected.id])
 
     const changePrice = event => {
-        if(!/^\d+(\.\d{1,2})?$/.test(event.target.value)) {
+        if (!/^\d+(\.\d{1,2})?$/.test(event.target.value)) {
             setIsDisabled(true)
             setErrorPrice(true);
         }
-        else{
+        else {
             setIsDisabled(false)
             setErrorPrice(false);
         }
-        
     }
+
+    const isCheckedFamilly = () => {
+        // let isFound;
+
+        // if (productSelected.id !== "") {
+
+        //     isFound = productSelected.productFamilies.some(element => {
+        //         if (element === famillyId.productFamilyId) {
+        //             return true
+        //         }
+        //         return false
+        //     })
+        // }
+        return  true;
+    }
+
+    const handleChange = (event) => {
+
+    };
+
     return (
         <Box
             // onSubmit={validationFormulaire}
@@ -62,11 +112,28 @@ const ProductForm = ({ productSelected }) => {
                 <TextField required className="pathImg" label="Chemin" variant="outlined" value={productSelected.imageUrl} />
             </div>
 
-        
+            <div className="FamillyCheckBox">
+            {(!isLoading) && (
+
+                <FormGroup>
+                    {
+                    famillyList.data.map((familly) => {
+                            return <FormControlLabel key={familly.id} label={"dfgh"}
+                                control={<Checkbox key={familly.id} checked={isCheckedFamilly()} />}/>
+                            
+                           
+                        })
+                    }
+                        
+                </FormGroup>
+                    )}
+
+            </div>
+
             <OutlinedInput
                 className="price"
                 error={errorPrice}
-                value={productSelected.price}
+                value={productSelected.currentPrice}
                 // onKeyPress={onlyNumber}
                 // onChange={changePrice}
                 onBlur={changePrice}
